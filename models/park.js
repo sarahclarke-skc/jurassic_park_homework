8ab75db14e1a66c38827b87e4a732fae4f045452
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-24 18:43:49 +0100

    Tests checking parameters are passing

---
+++ models/park.js
@@ -0,0 +1,7 @@
+const Park = function (name, ticketPrice) {
+    this.name = name;
+    this.ticketPrice = ticketPrice;
+    this.collectionOfDinos = [];
+}
+
+module.exports = Park;
---
+++ specs/park_spec.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+let park;
+
+  beforeEach(function () {
+    park = new Park('Jurassic Park', 10)
+  });
+
+  it('should have a name', function () {
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurassic Park');
+  });
+
+  it('should have a ticket price', function () {
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 10)
+  });
+
+  it('should have a collection of dinosaurs', function () {
+    const actual = park.collectionOfDinos;
+    assert.deepStrictEqual(actual, [])
+  });
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to calculate the total number of visitors per day');
+
+  it('should be able to calculate the total number of visitors per year');
+
+  it('should be able to calculate total revenue for one year');
+
+});
